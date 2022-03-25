0efe74c2a49e85198c5febd842ff0198d46c4a46
joymohanta <joymohanta06@gmail.com>
2022-03-26 05:27:05 +0600

    title changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="font-design">Exclusive Phones</h1>
+      <h1 className="font-design">Exclusive Watches</h1>
       <h3>Select 4 watches</h3>
     </div>
   );

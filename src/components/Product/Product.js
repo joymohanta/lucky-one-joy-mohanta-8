fa512ceea589fa6a7592603ec2145803104911b2
joymohanta <joymohanta06@gmail.com>
2022-03-26 08:55:48 +0600

    mostly done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
   //   console.log(props);
-  const { img, name, price } = props.product;
+  const { product, addToCartHandler } = props;
+  const { img, name, price } = product;
   return (
     <div className="item-container">
       <img src={img} alt="" />
-      <h3> {name} </h3>
-      <h4> ${price} </h4>
-      <button className="btn-cart">
-        <p>Add To Cart</p>
+      <h2> {name} </h2>
+      <h3> ${price} </h3>
+      <button onClick={() => addToCartHandler(product)} className="btn-cart">
+        <p className="btn-text">Add To Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
       </button>
     </div>
   );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,21 +4,39 @@ import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [watch, setWatch] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  const addToCartHandler = (product) => {
+    console.log(product.name);
+    // const watchName = product.name;
+    const newWatch = [...watch, product];
+    setWatch(newWatch);
+  };
   return (
     <div className="shop-container">
       <div className="product-container">
         {products.map((product) => (
-          <Product product={product} key={product.id}></Product>
+          <Product
+            product={product}
+            addToCartHandler={addToCartHandler}
+            key={product.id}
+          ></Product>
         ))}
       </div>
-
       <div className="cart-container">
-        <h3>Selected Watches</h3>
+        <h3>Selected Watches {watch.length} </h3>
+        <p>Name: {watch.name} </p>
+        <button className="cart-button">
+          <p className="button-text">PICK 1 FOR ME</p>
+        </button>
+        <br />
+        <button className="cart-button">
+          <p className="button-text">CHOOSE AGAIN</p>
+        </button>
       </div>
     </div>
   );
